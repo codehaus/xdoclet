28bfaa70641188f0e21b000a5482c552bb565425
topping <topping@13fd1478-af3e-0410-90e7-f6ce8690fa2d>
2004-11-06 17:06:35 +0000

    Refactor DocletTagFactory

--- trunk/xdoclet/src/main/java/org/xdoclet/ConfigurableDocletTagFactory.java
+++ trunk/xdoclet/src/main/java/org/xdoclet/ConfigurableDocletTagFactory.java
@@ -1,18 +1,8 @@
 package org.xdoclet;
 
 import com.thoughtworks.qdox.model.DefaultDocletTag;
+import com.thoughtworks.qdox.model.DefaultDocletTagFactory;
 import com.thoughtworks.qdox.model.DocletTag;
-import com.thoughtworks.qdox.model.DocletTagFactory;
-import com.thoughtworks.qdox.model.AbstractJavaEntity;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * A paranoid tag factory that will remember
@@ -25,10 +15,7 @@ import java.util.Set;
  * @author Aslak Helles&oslash;y
  * @version $Revision$
  */
-public class ConfigurableDocletTagFactory implements DocletTagFactory {
-    private List unknownTags = new ArrayList();
-    private final Map registeredTags = new HashMap();
-
+public class ConfigurableDocletTagFactory extends DefaultDocletTagFactory {
     public ConfigurableDocletTagFactory() {
         String[] defaultTags = new String[] {
             "param",
@@ -49,61 +36,5 @@ public class ConfigurableDocletTagFactory implements DocletTagFactory {
 
     public DocletTag createDocletTag(String tag, String text) {
         throw new UnsupportedOperationException();
-    }
-
-    public DocletTag createDocletTag(String tag, String text, AbstractJavaEntity context, int lineNumber) {
-        Class tagClass = (Class) registeredTags.get(tag);
-
-        boolean isKnown = true;
-        if( tagClass == null ) {
-            tagClass = DefaultDocletTag.class;
-            isKnown = false;
-        }
-        try {
-            Constructor newTag = tagClass.getConstructor(new Class[] {String.class, String.class, AbstractJavaEntity.class, Integer.TYPE});
-            DocletTag result = (DocletTag) newTag.newInstance(new Object[]{tag, text, context, new Integer(lineNumber)});
-
-            if (!isKnown) {
-                unknownTags.add(result);
-            }
-            return result;
-        } catch (ClassCastException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("No (String, String, AbstractJavaEntity, int) constructor in " + tagClass.getName());
-        } catch (SecurityException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e.getTargetException());
-        }
-    }
-
-    public void registerTag(String tagName, Class tagClass) {
-        registeredTags.put(tagName, tagClass != null ? tagClass : DefaultDocletTag.class);
-    }
-
-    public void registerTags(Map tags) {
-        Set tagNames = tags.keySet();
-        for (Iterator iterator = tagNames.iterator(); iterator.hasNext();) {
-            String tagName = (String) iterator.next();
-            registerTag(tagName, (Class) tags.get(tagName));
-        }
-    }
-
-    public List getUnknownTags() {
-        return unknownTags;
-    }
-
-    public void printUnknownTags() {
-        for (Iterator iterator = unknownTags.iterator(); iterator.hasNext();) {
-            DocletTag docletTag = (DocletTag) iterator.next();
-            System.out.println("Unknown tag: @" + docletTag.getName() + " in " + XDocletTag.getLocation(docletTag) + " (line " + docletTag.getLineNumber() + ")");
-        }
     }
 }
