9dded4f942913f3a6703c1695458ce2a513f20a7
nobody <nobody@13fd1478-af3e-0410-90e7-f6ce8690fa2d>
2005-06-05 09:02:16 +0000

    This commit was manufactured by cvs2svn to create tag 'XDOCLET_2_0_3'.

---
+++ tags/XDOCLET_2_0_3/xdoclet/src/main/java/org/xdoclet/XDocletTag.java
@@ -0,0 +1,69 @@
+package org.xdoclet;
+
+import com.thoughtworks.qdox.model.AbstractJavaEntity;
+import com.thoughtworks.qdox.model.DefaultDocletTag;
+import com.thoughtworks.qdox.model.JavaField;
+import com.thoughtworks.qdox.model.JavaMethod;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public abstract class XDocletTag extends DefaultDocletTag {
+    protected boolean isOnConstructor;
+    protected boolean isOnMethod;
+    protected boolean isOnField;
+    protected boolean isOnClass;
+
+    protected XDocletTag(String name, String value, AbstractJavaEntity context, int lineNumber) {
+        super(name, value, context, lineNumber);
+        if (JavaMethod.class.isAssignableFrom(getContext().getClass())) {
+            JavaMethod method = (JavaMethod) getContext();
+            isOnConstructor = method.isConstructor();
+            isOnMethod = !method.isConstructor();
+        } else if (JavaField.class.isAssignableFrom(getContext().getClass())) {
+            isOnField = true;
+        } else {
+            isOnClass = true;
+        }
+        validateLocation();
+    }
+
+    protected abstract void validateLocation();
+
+    public final void bomb(String message) {
+        throw new RuntimeException("@" + getName() + " " + getValue() + "\n in " + org.generama.ConfigurableDocletTagFactory.getLocation(this) + " (line " + getLineNumber() + "):\n" + message);
+    }
+
+    public boolean isOnConstructor() {
+        return isOnConstructor;
+    }
+
+    public void setOnConstructor(boolean onConstructor) {
+        isOnConstructor = onConstructor;
+    }
+
+    public boolean isOnMethod() {
+        return isOnMethod;
+    }
+
+    public void setOnMethod(boolean onMethod) {
+        isOnMethod = onMethod;
+    }
+
+    public boolean isOnField() {
+        return isOnField;
+    }
+
+    public void setOnField(boolean onField) {
+        isOnField = onField;
+    }
+
+    public boolean isOnClass() {
+        return isOnClass;
+    }
+
+    public void setOnClass(boolean onClass) {
+        isOnClass = onClass;
+    }
+}
