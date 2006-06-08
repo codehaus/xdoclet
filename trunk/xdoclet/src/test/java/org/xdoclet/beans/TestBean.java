8973006568be09250ff64a00938136febc472d53
ko5tik <ko5tik@13fd1478-af3e-0410-90e7-f6ce8690fa2d>
2006-06-08 17:59:07 +0000

    resubmitted missing files (due to server crash )

---
+++ trunk/xdoclet/src/test/java/org/xdoclet/beans/TestBean.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2005
+ * XDoclet Team
+ * All rights reserved.
+ */
+package org.xdoclet.beans;
+
+/**
+ * @tag2 tagValue
+ */
+public class TestBean {
+    public TestBean() {
+        // empty
+    }
+}
